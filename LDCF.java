fbaf8f222481fe4fe97443c946f6871b1e7cd792
jsalcor <jsacorchado@gmail.com>
2024-05-25 19:42:00 +0200

    archivo main ejecutable

--- LDCF.java
+++ LDCF.java
@@ -1,4 +1,4 @@
-package bioinformatics.archivos_Sofia;
+package bioinformatics;
 
 import java.util.Random;
 
@@ -176,51 +176,14 @@ public class LDCF {
         return count;
     }
 
-    public static String[] generateKMers(String gnome, int k) {
-        String[] kmers = new String[gnome.length() - k + 1];
+    // public static String[] generateKMers(String gnome, int k) {
+    //     String[] kmers = new String[gnome.length() - k + 1];
 
-        for (int i = 0; i < gnome.length() - k + 1; i++) {
-            kmers[i] = gnome.substring(i, i + k);
-        }
+    //     for (int i = 0; i < gnome.length() - k + 1; i++) {
+    //         kmers[i] = gnome.substring(i, i + k);
+    //     }
 
-        return kmers;
-    }
+    //     return kmers;
+    // }
 
-    public static void main(String[] args) {
-        // Load E. coli genome
-        String eColiGnome = ""
-        + "GGTCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTG";
-
-        LDCF ldcf = new LDCF(1024, 4, 500);
-
-        int[] kValues = {10, 20, 50, 100, 200};
-        for (int k : kValues) {
-            String[] kmers = generateKMers(eColiGnome, k);
-
-            for (String kmer : kmers) {
-                int hashCode = kmer.hashCode();
-                ldcf.insert(hashCode);
-            }
-
-            for (String kmer : kmers) {
-                ldcf.insert(Integer.parseInt(kmer, 2));
-            }
-
-            System.out.println("k = " + k);
-            System.out.println("Number of collisions: " + ldcf.numCollisions());
-            System.out.println();
-
-            for (String kmer : kmers) {
-                ldcf.delete(Integer.parseInt(kmer, 2));
-            }
-            
-            System.out.println("Number of collisions: " + ldcf.numCollisions());
-            System.out.println();
-
-            // Example lookup
-            String sequence = "ACGTACGTAC";
-            boolean exists = ldcf.lookup(sequence.hashCode());
-            System.out.println("Sequence exists: " + exists);
-        }
-    }
 }
---
+++ mainclass.java
@@ -0,0 +1,53 @@
+package bioinformatics;
+
+public class mainclass {
+
+    public static String[] generateKMers(String gnome, int k) {
+        String[] kmers = new String[gnome.length() - k + 1];
+
+        for (int i = 0; i < gnome.length() - k + 1; i++) {
+            kmers[i] = gnome.substring(i, i + k);
+        }
+
+        return kmers;
+    }
+
+    public static void main(String[] args) {
+        // Load E. coli genome
+        String eColiGnome = ""
+        // + "GGTCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTGCTG";
+        + "GTACTCAGTGTACGATCAGCTACCGACT";
+
+        LDCF ldcf = new LDCF(1024, 4, 500);
+
+        int[] kValues = {10, 20, 50, 100, 200};
+        for (int k : kValues) {
+            String[] kmers = generateKMers(eColiGnome, k);
+
+            for (String kmer : kmers) {
+                int hashCode = kmer.hashCode();
+                ldcf.insert(hashCode);
+            }
+
+            for (String kmer : kmers) {
+                ldcf.insert(Integer.parseInt(kmer, 2));
+            }
+
+            System.out.println("k = " + k);
+            System.out.println("Number of collisions: " + ldcf.numCollisions());
+            System.out.println();
+
+            for (String kmer : kmers) {
+                ldcf.delete(Integer.parseInt(kmer, 2));
+            }
+            
+            System.out.println("Number of collisions: " + ldcf.numCollisions());
+            System.out.println();
+
+            // Example lookup
+            String sequence = "ACGTACGTAC";
+            boolean exists = ldcf.lookup(sequence.hashCode());
+            System.out.println("Sequence exists: " + exists);
+        }
+    }
+}
