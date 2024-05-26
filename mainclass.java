4e8f1a5146d95ef08da1baa2a129c8810f31fd80
sofiaprlsd <sofia.peralesdiez@gmail.com>
2024-05-26 16:47:29 +0200

    Funciona con kmers

--- mainclass.java
+++ mainclass.java
@@ -7,6 +7,13 @@ import java.io.IOException;
 public class mainclass {
 
     public static String[] generateKMers(String gnome, int k) {
+        if (k <= 0) {
+            throw new IllegalArgumentException("k must be a positive integer");
+        }
+        if (gnome == null || gnome.length() < k) {
+            throw new IllegalArgumentException("gnome must be a string of length at least k");
+        }
+
         String[] kmers = new String[gnome.length() - k + 1];
 
         for (int i = 0; i < gnome.length() - k + 1; i++) {
@@ -18,8 +25,7 @@ public class mainclass {
 
     public static void main(String[] args) {
         // Load E. coli genome
-        String eColiGnome = ""
-        + "GTACTCAGTGTACGATCAGCTACCGACT";
+        String genome = "GTACTCAGTGTACGATCAGCTACCGACT";
 
         // String filePath = "bioinformatics/datasets/GCF_000008865.2_ASM886v2_genomic.fna";
         // String genome = null;
@@ -35,35 +41,35 @@ public class mainclass {
 
         LDCF ldcf = new LDCF(1024, 4, 500);
 
-        int[] kValues = {10, 20, 50, 100, 200};
+        int[] kValues = {5, 10};
         for (int k : kValues) {
-            String[] kmers = generateKMers(eColiGnome, k);
-            // String[] kmers = generateKMers(genome, k);
+            try {
+                String[] kmers = generateKMers(genome, k);
 
-            for (String kmer : kmers) {
-                int hashCode = kmer.hashCode();
-                ldcf.insert(hashCode);
+                for (String kmer : kmers) {
+                    int hashCode = kmer.hashCode();
+                    ldcf.insert(hashCode);
+                }
+
+                // for (String kmer : kmers) {
+                //     ldcf.insert(Integer.parseInt(kmer, 2));
+                // }
+
+                System.out.println("k = " + k);
+                System.out.println("Number of collisions: " + ldcf.numCollisions());
+                System.out.println();
+                
+                // System.out.println("Number of collisions: " + ldcf.numCollisions());
+                // System.out.println();
+
+                // Example lookup
+                // "TCCTTGGATGAGCGCG"
+                String sequence = "GACT";
+                boolean exists = ldcf.lookup(sequence.hashCode());
+                System.out.println("Sequence exists: " + exists);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-
-            // for (String kmer : kmers) {
-            //     ldcf.insert(Integer.parseInt(kmer, 2));
-            // }
-
-            System.out.println("k = " + k);
-            System.out.println("Number of collisions: " + ldcf.numCollisions());
-            System.out.println();
-
-            // for (String kmer : kmers) {
-            //     ldcf.delete(Integer.parseInt(kmer, 2));
-            // }
-            
-            // System.out.println("Number of collisions: " + ldcf.numCollisions());
-            // System.out.println();
-
-            // Example lookup
-            String sequence = "ACGTACGTAC";
-            boolean exists = ldcf.lookup(sequence.hashCode());
-            System.out.println("Sequence exists: " + exists);
         }
     }
 }
